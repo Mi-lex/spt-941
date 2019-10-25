255193de35e9230f47b843ed85f6048433ceacfb
Milex <mishin-lex@mail.ru>
2019-10-25 11:29:53 +0300

    Add id to the device record

--- database/migrations/2019_10_25_042600_create_devices_table.php
+++ database/migrations/2019_10_25_042600_create_devices_table.php
@@ -14,6 +14,7 @@ class CreateDevicesTable extends Migration
     public function up()
     {
         Schema::create('devices', function (Blueprint $table) {
+            $table->increments('id');
             $table->ipAddress('ip');
             $table->smallInteger('port')->unsigned();
             $table->smallInteger('device_address')->unsigned();
